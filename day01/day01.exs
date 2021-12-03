971881b805ad966219e3c2742b2cdcfdd3e5f772
Marco Polita <marco.polita@kloeckner.com>
2021-12-03 15:22:16 +0100

    day1 part2

--- day01/day01.exs
+++ day01/day01.exs
@@ -2,14 +2,20 @@ defmodule Increments do
   def count(file_name) do
     file_name
     |> File.stream!()
+    |> Enum.map(fn x ->
+      {value, _} = Integer.parse(x)
+      value
+    end)
+    |> Stream.chunk_every(3, 1)
+    |> Enum.map(&Enum.sum/1)
     |> Stream.chunk_every(2, 1)
     |> Enum.filter(fn measure -> Enum.count(measure) > 1 end)
     |> Enum.count(fn [measure_1, measure_2] ->
-      Integer.parse(measure_1) < Integer.parse(measure_2)
+      measure_1 < measure_2
     end)
   end
 end
 
-7 = Increments.count("test_input.txt")
+5 = Increments.count("test_input.txt")
 
 IO.puts(Increments.count("input.txt"))
