4c1674f6c99e9c30d1d9cdfc181a5b089586e457
Marco Polita <marco.polita@kloeckner.com>
2021-12-03 15:38:04 +0100

    day 2 part 2

--- day02/day02.exs
+++ day02/day02.exs
@@ -1,27 +1,27 @@
 defmodule Submarine do
-  def move({horizontal, depth}, "forward", units) do
-    {horizontal + units, depth}
+  def move({horizontal, depth, aim}, "forward", units) do
+    {horizontal + units, depth + aim * units, aim}
   end
 
-  def move({horizontal, depth}, "down", units) do
-    {horizontal, depth + units}
+  def move({horizontal, depth, aim}, "down", units) do
+    {horizontal, depth, aim + units}
   end
 
-  def move({horizontal, depth}, "up", units) do
-    {horizontal, depth - units}
+  def move({horizontal, depth, aim}, "up", units) do
+    {horizontal, depth, aim - units}
   end
 
   def execute_commands(file) do
     file
     |> File.stream!()
     |> Enum.map(&String.split/1)
-    |> Enum.reduce({0, 0}, fn [direction, units], acc ->
+    |> Enum.reduce({0, 0, 0}, fn [direction, units], acc ->
       move(acc, direction, String.to_integer(units))
     end)
   end
 end
 
-{15, 10} = Submarine.execute_commands("test_input.txt")
+{15, 60, _} = Submarine.execute_commands("test_input.txt")
 
-{x, depth} = Submarine.execute_commands("input.txt")
+{x, depth, _} = Submarine.execute_commands("input.txt")
 IO.puts(x * depth)
