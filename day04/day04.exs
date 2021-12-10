d6a8418b77ed29a9e497ef92f824fc2bd6f91c9a
Marco Polita <marco.polita@kloeckner.com>
2021-12-10 16:16:19 +0100

    day04 part 1

--- day04/day04.exs
+++ day04/day04.exs
@@ -1,12 +1,80 @@
 defmodule Bingo do
-  def read_input(filename) do
-    lines =
+  @board_size 5
+  def calculate_winning(filename) do
+    [draws | boards] =
       filename
       |> File.stream!()
-      |> Enum.map(fn line -> line end)
+      |> Enum.map(&String.trim/1)
+      |> Enum.filter(fn line -> line != "" end)
 
-    %{draws: hd(lines)}
+    draws =
+      draws
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+
+    boards =
+      boards
+      |> Enum.map(fn line ->
+        line
+        |> String.split()
+        |> Enum.map(fn number -> {String.to_integer(number), false} end)
+      end)
+      |> Enum.chunk_every(@board_size)
+      |> Enum.map(fn lines -> lines |> Enum.concat() end)
+
+    Enum.reduce_while(draws, boards, fn draw, acc ->
+      boards =
+        acc
+        |> Enum.map(fn board -> mark_draw_on_board(draw, board) end)
+
+      winning_board =
+        boards
+        |> Enum.find(&board_won?/1)
+
+      if winning_board != nil, do: {:halt, {draw, winning_board}}, else: {:cont, boards}
+    end)
+    |> calculate_score()
+  end
+
+  def mark_draw_on_board(draw, board) do
+    board
+    |> Enum.map(fn {value, marked} -> {value, marked || value == draw} end)
+  end
+
+  def calculate_score({draw, winning_board}) do
+    unmarked_sum =
+      winning_board
+      |> Enum.filter(fn {_, marked} -> !marked end)
+      |> Enum.reduce(0, fn {value, _}, acc -> acc + value end)
+
+    unmarked_sum * draw
+  end
+
+  def board_won?(board) do
+    rows =
+      board
+      |> Enum.chunk_every(@board_size)
+
+    row_won =
+      rows
+      |> Enum.any?(fn row ->
+        Enum.count(row, fn {_, marked} -> marked == true end) == @board_size
+      end)
+
+    column_won =
+      Enum.map(0..(@board_size - 1), fn col ->
+        Enum.map(0..(@board_size - 1), fn row ->
+          Enum.at(board, col + row * @board_size)
+        end)
+      end)
+      |> Enum.any?(fn col ->
+        Enum.count(col, fn {_, marked} -> marked == true end) == @board_size
+      end)
+
+    row_won || column_won
   end
 end
 
-%{draws: "7,4,9" <> _} = Bingo.read_input("test_input.txt")
+4512 = Bingo.calculate_winning("test_input.txt")
+
+IO.puts(Bingo.calculate_winning("input.txt"))
